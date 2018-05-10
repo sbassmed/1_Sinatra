42c3c45cc717e58cfb9d4677e7856e70561bdc94
Sebastian <sebastiancamilomarin@gmail.com>
2018-05-10 01:11:28 -0500

    Update solution.rb
    codigo secreto blog

--- solution.rb
+++ solution.rb
@@ -1,19 +1,6 @@
 require 'sinatra'
 
-get '/makers/:nombre' do
-  "Hola #{params[:nombre].capitalize}!"
+get '/' do
+ "endHcUy6Re2LLBRtj"
 end
 
-get '/query' do
-  unless params[:nombre] != nil && params[:nombre].capitalize == "Juan"
-    "Hola desconocido!"
-  else
-    "<h1>Hola #{params[:nombre]}!</h1>"
-  end
-end
-get'/' do
-  erb :index
-end
- post '/saludo' do
-   "<h1>Hola #{params[:nombre]}</h1>"
- end
