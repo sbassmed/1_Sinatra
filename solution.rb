3067fab906268322a8cef292691c33b1da66486a
Sebastian <sebastiancamilomarin@gmail.com>
2018-05-09 23:56:32 -0500

    Update solution.rb
    implementando formulario y vistas

--- solution.rb
+++ solution.rb
@@ -4,10 +4,16 @@ get '/makers/:nombre' do
   "Hola #{params[:nombre].capitalize}!"
 end
 
-get '/' do
+get '/query' do
   unless params[:nombre] != nil && params[:nombre].capitalize == "Juan"
     "Hola desconocido!"
   else
     "<h1>Hola #{params[:nombre]}!</h1>"
   end
 end
+get'/' do
+  erb :index
+end
+ post '/saludo' do
+   "<h1>Hola #{params[:nombre]}</h1>"
+ end
